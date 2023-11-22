5127b78a8d973cb44a426b517f988f0bc922042a
Vishnuprasanth-2002 <s.vishnuprasanth14@gmail.com>
2023-11-23 01:19:58 +0530

    commit

--- app/routes/amazon.route.js
+++ app/routes/amazon.route.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const { validate } = require("../middlewares/validate.middleware");
-const { loginController ,getAccountController} = require("../controllers/amazon.controllers");
+const {
+  loginController,
+  getAccountController,
+} = require("../controllers/amazon.controllers");
 const { loginSchema } = require("../validations/schemas");
 
-
 router.post("/login", validate(loginSchema), loginController);
-router.get("/account/:id",validate(),getAccountController)
+router.get("/account/:id", getAccountController);
 
 module.exports = router;
